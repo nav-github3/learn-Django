9d0a2f0a9c99ce62f4a3ae61f7e04c1f899d3b4b
navneet <navneetlonare77@gmail.com>
2025-06-28 11:46:06 +0530

    Add views for home, about, and contact pages; update URLs and settings; create index.html and style.css

--- chaiaurcode/chaiaurcode/views.py
+++ chaiaurcode/chaiaurcode/views.py
@@ -1,19 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
-def homeResquest(request):
-	return HttpResponse("You are at chai aur djaongo Home page"); 
+
+def home(request):
+	#return HttpResponse("Hello, world ! you are at Django page")
+	return render(request , 'website/index.html')
 
 def about(request):
-	return HttpResponse("This is the about page of the djaongo"); 
+	return HttpResponse("Hellow  this is about  page of the application ")
 
-def contact(request): 
-	return HttpResponse("This is the contact page of the djapgo"); 
-
-def service_termical(request) : 
-	return HttpResponse("This is the service termical page of the djapgo");
-
-
-
-def about78(request):
-	return HttpResponse("This is the about page of the djaongo"); 
-
+def contact(request):
+	return HttpResponse("This  is  the contact page of the the application ")
