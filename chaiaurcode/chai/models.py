665eee780c40f152472b30c40964c67ec2d48805
navneet <navneetlonare77@gmail.com>
2025-07-07 04:41:59 +0530

    Add chaiVariety model with name, image, date_added, and type fields; update settings for media files

--- chaiaurcode/chai/models.py
+++ chaiaurcode/chai/models.py
@@ -1,14 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
-class Article(models.Model):
-    author = models.ForeignKey(
-        'auth.User',
-        on_delete=models.CASCADE,
-    )
-    title = models.CharField(max_length=200)
-    text = models.TextField()
+class chaiVariety(models.Model):
+    name  = models.CharField(max_length= 100)
+    image = models.ImageField(upload_to='chais/')
+    date_added = models.DateTimeField(default=timezone.now)
+    CHAI_TYPE_CHOICE = [
+        ('ML', 'MASALA'), 
+        ('GR', 'GINGER'), 
+        ('KL', 'PLAIN'),
+        ('EL', 'ELACHI')
+    ]
 
-
-    def __str__(self):
-        return self.title
+    type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICE)
+    
+    
+    
+   # def __str__(self):
+    #    return self.name
